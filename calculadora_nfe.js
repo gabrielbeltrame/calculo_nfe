00e892aa0fd820fce89e1e579a8300392382334a
Gabriel Beltrame Silva <122296554+gabrielbeltrame@users.noreply.github.com>
2023-06-06 00:55:17 +0000

    Finalizado

--- calculadora_nfe.js
+++ calculadora_nfe.js
@@ -1,10 +1,5 @@
 "use strict";
-const a = 'Olá';
-console.log(a);
-const chave = '3520032576926600071055000005116055121545955'.trim();
-const vetorChave = chave.split('');
-console.log(chave + calculaDigito(calculaSomatoria(vetorChave, gerarVetorFatorMultiplicador(vetorChave.length))));
-console.log(geradoraNumeroNFe());
+console.log(geradorNumeroNFe());
 function gerarVetorFatorMultiplicador(tamanhoArray) {
     const arrayResultado = [];
     let arrayFatorMultiplicador = [9, 8, 7, 6, 5, 4, 3, 2];
@@ -28,10 +23,27 @@ function calculaDigito(somatoria) {
         return 0;
     return modulo;
 }
-function geradoraNumeroNFe() {
+function geradorNumeroNFe() {
+    const uf = getRandomUF().toString();
     const dataHoje = new Date();
-    const dataChave = ('0' + dataHoje.getDate()).slice(-2) + ('0' + dataHoje.getMonth()).slice(-2);
-    console.log(dataChave);
-    return '';
+    const dataChave = dataHoje.getFullYear().toString().slice(-2) + ('0' + dataHoje.getMonth()).slice(-2).toString();
+    const cnpj = '07829401000106';
+    const modelo = ('00' + getRandomIntInclusive(1, 99)).slice(-2);
+    const serie = ('000' + getRandomIntInclusive(0, 999)).slice(-3);
+    const numero = ('000000000' + getRandomIntInclusive(1, 999999999)).slice(-9);
+    const tipo = '1';
+    const codigo = ('00000000' + getRandomIntInclusive(1, 99999999)).slice(-8);
+    const chave = (uf + dataChave + cnpj + modelo + serie + numero + tipo + codigo).trim();
+    const vetorChave = chave.split('');
+    return chave + calculaDigito(calculaSomatoria(vetorChave, gerarVetorFatorMultiplicador(vetorChave.length)));
+}
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function getRandomUF() {
+    const ufs = [11, 12, 13, 14, 15, 16, 17, 21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 32, 33, 35, 41, 42, 43, 50, 51, 52, 53];
+    return ufs[getRandomIntInclusive(0, ufs.length)];
 }
 //# sourceMappingURL=calculadora_nfe.js.map
--- calculadora_nfe.ts
+++ calculadora_nfe.ts
@@ -1,12 +1,4 @@
-const a = 'Olá'
-
-console.log(a);
-
-const chave = '3520032576926600071055000005116055121545955'.trim()
-const vetorChave = chave.split('')
-
-console.log(chave+calculaDigito(calculaSomatoria(vetorChave, gerarVetorFatorMultiplicador(vetorChave.length))))
-console.log(geradoraNumeroNFe())
+console.log(geradorNumeroNFe())
 
 function gerarVetorFatorMultiplicador(tamanhoArray: number): number[] {
     const arrayResultado: number[] = [];
@@ -36,11 +28,33 @@ function calculaDigito(somatoria: number): number {
     return modulo;
 }
 
-function geradoraNumeroNFe(): string {
+function geradorNumeroNFe(): string {
+    const uf = getRandomUF().toString()
+    
     const dataHoje = new Date();
-    const dataChave = ('0' + dataHoje.getDate()).slice(-2) + ('0' + dataHoje.getMonth()).slice(-2);
+    const dataChave = dataHoje.getFullYear().toString().slice(-2) + ('0' + dataHoje.getMonth()).slice(-2).toString();
+    const cnpj = '07829401000106'
+    const modelo = ('00' + getRandomIntInclusive(1, 99)).slice(-2);
+    const serie = ('000' + getRandomIntInclusive(0, 999)).slice(-3);
+    const numero = ('000000000' + getRandomIntInclusive(1, 999999999)).slice(-9);
+    const tipo = '1'
+    const codigo = ('00000000' + getRandomIntInclusive(1, 99999999)).slice(-8);
 
-    console.log(dataChave);
+    const chave = (uf + dataChave + cnpj + modelo + serie + numero + tipo + codigo).trim()
+    const vetorChave = chave.split('')
 
-    return ''
+    return chave + calculaDigito(calculaSomatoria(vetorChave, gerarVetorFatorMultiplicador(vetorChave.length)))
+}
+
+function getRandomIntInclusive(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRandomUF(): number {
+    const ufs = [11,12,13,14,15,16,17,21,22,23,24,25,26,27,28,29,31,32,33,35,41,42,43,50,51,52,53]
+
+    return ufs[getRandomIntInclusive(0, ufs.length)]
+
 }
